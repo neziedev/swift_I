e62f7e289db704b6644c02446eef7844b5d1b111
neziedev <devnezie@gmail.com>
2023-02-01 03:13:58 -0500

    completes step one

--- swift_I.playground/Contents.swift
+++ swift_I.playground/Contents.swift
@@ -1,1 +1,16 @@
 // get started here
+
+// step 1
+
+let leak_1 = (name: "Tom Cruise", severity: 8, compromised: false);
+let leak_2 = (name: "John Voight", severity: 9, compromised: true);
+let leak_3 = (name: "Emmanuelle Beart", severity: 5, compromised: false);
+let leak_4 = (name: "Henry Czerny", severity: 10, compromised: true);
+let leak_5 = (name: "Jean Reno", severity: 4, compromised: false);
+let leak_6 = (name: "Ving Rhames", severity: 4, compromised: false);
+let leak_7 = (name: "Kristin Scott Thomas", severity: 5, compromised: true);
+let leak_8 = (name: "Vanessa Redgrave", severity: 4, compromised: false);
+let leak_9 = (name: "Ingeborga Dapkūnaitė", severity: 5, compromised: true);
+let leak_10 = (name: "Emilio Estevez", severity: 6, compromised: true);
+let leak_11 = (name: "Dale Dye", severity: 9, compromised: false);
+
